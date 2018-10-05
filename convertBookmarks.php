4e42352cf212daee90f8c4289277790b5430b5af
magicmulder <github@muldermedia.de>
2018-10-05 17:36:07 +0200

    Change: removed all debug output and unnecessary stuff

--- convertBookmarks.php
+++ convertBookmarks.php
@@ -2,7 +2,7 @@
 /**
 *	Chrome Bookmark Downloader 
 *
-*	v1.0
+*	v1.0.1
 *
 *	(c) 2018 magicmulder <developer@muldermedia.de>
 *
@@ -26,17 +26,11 @@ if (isset($argv[2])) {
 
 $handle = fopen($bookmarkFile, 'r');
 $handle2 = fopen($bookmarkScript, 'w');
-$depth = 0;
 $count = 0;
-$offset = (isset($_REQUEST['offset'])?$_REQUEST['offset']:1);
-$level[0] = '';
-$level[1] = '';
-$level[2] = '';
-$level[3] = '';
-#$line = '                 <DT><H3 ADD_DATE="1463170500" LAST_MODIFIED="1533323796">Yay</H3>';
+for ($i = 0; $i <= 10; $i++) {
+	$level[$i] = '';
+}
 while (($line = fgets($handle)) !== false) {
-	$count++;
-#	if ($count > 20) break;
 	$regExpLabel = '/(.+)<DT><H3 ADD_DATE="(.+)" LAST_MODIFIED="(.+)">(.+)<\/H3>/iU';
 	$regExpUrl = '/(.+)<A HREF="(.+)"/iU';
 	preg_match_all($regExpLabel, $line, $match);
@@ -47,18 +41,13 @@ while (($line = fgets($handle)) !== false) {
 		$label = str_replace('quot', '', $label);
 		$currLevel = (strlen($indent)-20)/4;
 		$level[$currLevel] = $label;
-		$level[$currLevel+1] = '';
-		$level[$currLevel+2] = '';
-		$level[$currLevel+3] = '';
-		$level[$currLevel+4] = '';
-		$level[$currLevel+5] = '';
-		$level[$currLevel+6] = '';
-#		echo  $currLevel. ": " . $label . "<br>";
-#fwrite($handle2, $currLevel. ": " . $label . chr(10).chr(13));
+		for ($i = 1; $i <= 10; $i++) {
+			$level[$currLevel+$i] = '';
+		}
 	} else {
-		if ($count < $offset) continue;
 		preg_match_all($regExpUrl, $line, $match2);
 		if (isset($match2[2][0])) {
+			$count++;
 			$link = $match2[2][0];
 			$currPath = $level[0];
 			if ($level[1] != '') $currPath .= '/' . $level[1];
@@ -71,12 +60,9 @@ while (($line = fgets($handle)) !== false) {
 			$generatedCall = $youtubeDL . $currPath . '/%(title)s [%(resolution)s] [%(id)s].%(ext)s" ' . $link;
 			fwrite($handle2, '#' . $count . chr(10));
 			fwrite($handle2, $generatedCall . chr(10));
-#echo $generatedCall . "<br>";			
-#			$arrStructure[$level[0]][$level[1]][$level[2]][$level[3]][] = $link;
 		}
 	}	
 }
-print_r($arrStructure);
 fclose($handle);
 fclose($handle2);
 chmod($bookmarkScript, '0777');
